0646d0e984a7884d18b924ea95940de6a85544e2
Amauri <hamauri@aol.com>
2022-10-27 13:56:37 -0700

    js to load two movies on page

--- assets/js/gamepage.js
+++ assets/js/gamepage.js
@@ -6,10 +6,45 @@
 
 // use 2nd api to grab movie info
 
+// temp variables
+var movieList = ['Dune', 'Memory'];
+var movieInfo = ['$108,327,830', '$7,329,043'];
+
+var moviePoster = [
+    'https://m.media-amazon.com/images/M/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg', 
+    'https://m.media-amazon.com/images/M/MV5BOGI5N2FhNzktZjZlNi00MmRjLWE1MmUtNjRlNzQyOGMzYjNhXkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_SX300.jpg'
+];
+
+function firstAPI () {
+    //grabs movie list depending on filters
+}
+
+function randomMovie() {
+    //function returns index of a random movie and removes from list
+}
+
+function getMovieInfo (index) {
+    //gets info on movie in index that was passed in
+}
+
 // load movie data onto page
+
+//call function using array of index
+loadGameOptionInfo([0, 1]);
+
+function loadGameOptionInfo (indexArray) {
+    var movieCardEl = document.querySelectorAll('.movie-card');
+    
+    for(var i = 0; i < indexArray.length; i++){
+        movieCardEl[i].children[0].textContent = `Box Office: ${movieList[indexArray[i]]}`;
+        movieCardEl[i].children[1].src = moviePoster[indexArray[i]];
+        movieCardEl[i].children[2].textContent = movieInfo[indexArray[i]] ;
+    }
+}
 
 // code handling user input when comparing both movies (includes loading data)
 
 // check input if correct continue or false stop game then load game over/store score
 
 // if continue move 2nd movie over and grab new movie to compare to
+
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,56 @@
+function doFetch() {
+    var genre = ['action', 'Family', 'Comedy', 'Animation', 'Horror', 'Crime', 'Sci-Fi', 'Romance', 'Thriller']
+
+    var urlImdb = `https://imdb-api.com/API/AdvancedSearch/${config.imdbApiKey}?title_type=feature&genres=${genre[0]}`
+
+    fetch(urlImdb).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+    }).then(function (data) {
+        doFetch2(data.results[30].title);
+        doFetch2(data.results[31].title)
+        console.log(data);
+    })
+
+    var url = "https://en.wikipedia.org/w/api.php";
+
+    var params = {
+        action: "query",
+        list: "search",
+        srsearch: "Lists of action films",
+        format: "json"
+    };
+
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function (key) { url += "&" + key + "=" + params[key]; });
+
+    fetch(url)
+        .then(function (response) { return response.json(); })
+        .then(function (response) {
+            if (response.query.search[0].title === "Nelson Mandela") {
+                console.log("Your search page 'Nelson Mandela' exists on English Wikipedia");
+            }
+            console.log(response);
+        })
+        .catch(function (error) { console.log(error); });
+
+}
+
+function doFetch2 (id) {
+    var title = id;
+    var urlOmbd = `http://www.omdbapi.com/?t=${title}&apikey=${config.omdbApiKey}`
+
+    fetch(urlOmbd).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+    }).then(function (data) {
+        console.log(data);
+    })
+}
+
 $('.ui.dropdown').dropdown();
+
+
+doFetch();
