23779c20f121ec0f5fdeb048728fc586c4e839bb
Amauri <hamauri@aol.com>
2022-10-28 20:58:03 -0700

    added code to save score in storage for scoreboard

--- assets/js/gameover.js
+++ assets/js/gameover.js
@@ -1,16 +1,30 @@
+
 function onLoad() {
     var results = JSON.parse(localStorage.getItem('results'));
     var scoreEls = document.querySelectorAll('.results');
     
     results.genreUsed = results.genreUsed.replace('_', ' ')
     results.genreUsed = results.genreUsed.charAt(0).toUpperCase() + results.genreUsed.slice(1);
-
+    
     results.gameTypeUsed = results.gameTypeUsed.replace('_', ' ');
     results.gameTypeUsed = results.gameTypeUsed.charAt(0).toUpperCase() + results.gameTypeUsed.slice(1);
-
+    
     scoreEls[0].textContent = results.finalScore;
     scoreEls[1].textContent = results.gameTypeUsed;
     scoreEls[2].textContent = results.genreUsed;
+}
+
+function saveScore(event) {
+    var inputEl = document.querySelector('.ui.input');
+    var results = JSON.parse(localStorage.getItem('results'));
+    var scoreboardList = JSON.parse(localStorage.getItem('scoreboard')) || []
+
+    inputEl.classList.add('disabled');
+    inputEl.children[1].classList.add('disabled');
+    results['userName'] = event.target.previousElementSibling.value;
+    scoreboardList.push(results);
+
+    localStorage.setItem('scoreboard', JSON.stringify(scoreboardList))
 }
 
 //goes back to main page
@@ -32,5 +46,6 @@ function navigateToScoreboard(event) {
 
 document.getElementById('back-btn').addEventListener('click', goHome)
 document.getElementById('scoreboard-btn').addEventListener('click', navigateToScoreboard)
+document.querySelector('.input button').addEventListener('click', saveScore)
 
 onLoad();
