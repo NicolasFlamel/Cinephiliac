39fd3b5113cc273c70d88ef5e46e51f4d6495448
Amauri <hamauri@aol.com>
2022-10-28 21:37:54 -0700

    scoreboard loading

--- assets/js/gameover.js
+++ assets/js/gameover.js
@@ -1,14 +1,14 @@
+var results = JSON.parse(localStorage.getItem('results'));
 
 function onLoad() {
-    var results = JSON.parse(localStorage.getItem('results'));
     var scoreEls = document.querySelectorAll('.results');
-    
+
     results.genreUsed = results.genreUsed.replace('_', ' ')
     results.genreUsed = results.genreUsed.charAt(0).toUpperCase() + results.genreUsed.slice(1);
-    
+
     results.gameTypeUsed = results.gameTypeUsed.replace('_', ' ');
     results.gameTypeUsed = results.gameTypeUsed.charAt(0).toUpperCase() + results.gameTypeUsed.slice(1);
-    
+
     scoreEls[0].textContent = results.finalScore;
     scoreEls[1].textContent = results.gameTypeUsed;
     scoreEls[2].textContent = results.genreUsed;
@@ -16,7 +16,6 @@ function onLoad() {
 
 function saveScore(event) {
     var inputEl = document.querySelector('.ui.input');
-    var results = JSON.parse(localStorage.getItem('results'));
     var scoreboardList = JSON.parse(localStorage.getItem('scoreboard')) || []
 
     inputEl.classList.add('disabled');
--- assets/js/scoreboard.js
+++ assets/js/scoreboard.js
@@ -1,3 +1,38 @@
+var boxTableEl = document.querySelector('#box-office-table tbody');
+var budgetTableEl = document.querySelector('#budget-table tbody');
+var ratingsTableEl = document.querySelector('#ratings-table tbody');
+var scoreboard = JSON.parse(localStorage.getItem('scoreboard')) || [];
+
+function onLoad() {
+    for (var i = 0; i < scoreboard.length; i++) {
+        displayOnBoard(scoreboard[i])
+    }
+}
+
+function displayOnBoard(score) {
+    var trEl = document.createElement('tr');
+    var nameEl = document.createElement('td');
+    var scoreEl = document.createElement('td');
+    var filtersEl = document.createElement('td');
+
+    nameEl.textContent = score.userName;
+    scoreEl.textContent = score.finalScore;
+    filtersEl.textContent = score.genreUsed;
+
+    trEl.appendChild(nameEl)
+    trEl.appendChild(scoreEl)
+    trEl.appendChild(filtersEl)
+
+    if (score.gameTypeUsed == 'Box office') {
+        boxTableEl.appendChild(trEl);
+    } else if (score.gameTypeUsed == 'Budget') {
+        budgetTableEl.appendChild(trEl);
+    } else if (score.gameTypeUsed == 'Ratings') {
+        ratingsTableEl.appendChild(trEl);
+    } else {
+        console.log('failed ln 17');
+    }
+}
 
 function goHome(event) {
     var url = window.location.href
@@ -7,4 +42,11 @@ function goHome(event) {
     window.location.replace(url);
 }
 
-document.getElementById('back-btn').addEventListener('click', goHome)
+function toggleFilters(event) {
+
+}
+
+document.getElementById('back-btn').addEventListener('click', goHome);
+document.getElementById('toggle-btn').addEventListener('click', toggleFilters);
+
+onLoad();
